c238ec9431ea89130624abb8fbb035b8a0c557d6
Jason Lai <jlai@users.noreply.github.com>
2024-07-22 11:03:44 -0700

    Add more copy to login page

--- src/components/login-box.tsx
+++ src/components/login-box.tsx
@@ -17,7 +17,7 @@ import Link from "next/link";
 import { ArrowDropDown } from "@mui/icons-material";
 
 import { redirectToLogin } from "@/api/auth";
-import { PRIVACY_POLICY_LINK } from "@/config";
+import { PRIVACY_POLICY_LINK, WEBSITE_NAME } from "@/config";
 
 function PermissionInfo({
   title,
@@ -39,7 +39,8 @@ function PermissionsTable() {
     <Table size="small">
       <TableBody>
         <PermissionInfo title="Activity">
-          Display steps, activities, calories burned, and other stats.
+          Display steps, activities, calories burned, and other stats, and log
+          manual activities.
         </PermissionInfo>
         <PermissionInfo title="Heart Rate">
           Display resting heart rate and heart rate zone graphs.
@@ -58,8 +59,12 @@ function PermissionsTable() {
           </Link>{" "}
           page to manually set your units.
         </PermissionInfo>
-        <PermissionInfo title="Sleep">Display sleep logs.</PermissionInfo>
-        <PermissionInfo title="Weight">Display weight history.</PermissionInfo>
+        <PermissionInfo title="Sleep">
+          Display sleep logs and manually log sleep.
+        </PermissionInfo>
+        <PermissionInfo title="Weight">
+          Display weight history and manually log weight.
+        </PermissionInfo>
       </TableBody>
     </Table>
   );
@@ -71,55 +76,90 @@ export default function LoginBox() {
   }, []);
 
   return (
-    <Container maxWidth="md">
-      <Typography variant="h3" marginBottom="24px">
-        Connect your Fitbit account
-      </Typography>
-      <div className="space-y-4">
-        <Typography variant="body1">
-          Connect your Fitbit account to display your daily stats on this
-          website.
+    <Container maxWidth="md" className="space-y-6">
+      <section>
+        <Typography variant="h4" marginBottom="24px">
+          Welcome to {WEBSITE_NAME}
         </Typography>
-        <Typography variant="body1">
-          No health or personal information will be shared with the operators of
-          this website or any third party. Data retrieved from the Fitbit Web
-          API will be stored solely in your browser&apos;s memory and offline
-          storage/cache.{" "}
-          {PRIVACY_POLICY_LINK && (
-            <span>
-              View our{" "}
-              <Link
-                href={PRIVACY_POLICY_LINK}
-                target="_blank"
-                className="underline"
-              >
-                privacy policy
-              </Link>{" "}
-              for more details.
-            </span>
-          )}
+        <div className="space-y-4">
+          <Typography variant="body1">
+            This website lets you explore your Fitbit exercise stats, log meals,
+            view runs and other activities, and more. This is a free interface
+            created by Fitbit users, for Fitbit users, and is not affiliated
+            with Fitbit or Google LLC.{" "}
+            <a href="/about" className="underline">
+              Learn more
+            </a>
+            .
+          </Typography>
+        </div>
+      </section>
+      <section>
+        <Typography variant="h5" marginBottom="24px">
+          Connect your Fitbit account
         </Typography>
-        <Accordion>
-          <AccordionSummary expandIcon={<ArrowDropDown />}>
-            How are permissions used?
-          </AccordionSummary>
-          <AccordionDetails>
-            <div className="space-y-2">
-              <p>
-                All permissions are optional, although some data may not be
-                displayed if you turn off certain permissions. You can add or
-                remove permissions later if you change your mind.
-              </p>
-              <PermissionsTable />
-            </div>
-          </AccordionDetails>
-        </Accordion>
-      </div>
-      <div className="mt-8 flex flex-col items-center">
-        <Button variant="contained" onClick={login}>
-          Connect with Fitbit
-        </Button>
-      </div>
+        <div className="space-y-4">
+          <Typography variant="body1">
+            Connect your Fitbit account to view your daily stats, historical
+            graphs and logs, and log new activities and other data.
+          </Typography>
+          <Typography variant="body1">
+            No health or personal information will be shared with the operators
+            of this website or any third party. Data retrieved from the Fitbit
+            Web API will be stored solely in your browser&apos;s memory and
+            offline storage/cache.{" "}
+            {PRIVACY_POLICY_LINK && (
+              <span>
+                View our{" "}
+                <Link
+                  href={PRIVACY_POLICY_LINK}
+                  target="_blank"
+                  className="underline"
+                >
+                  privacy policy
+                </Link>{" "}
+                for more details.
+              </span>
+            )}
+          </Typography>
+          <Accordion>
+            <AccordionSummary expandIcon={<ArrowDropDown />}>
+              How are permissions used?
+            </AccordionSummary>
+            <AccordionDetails>
+              <div className="space-y-2">
+                <p>
+                  All permissions are optional, although some data may not be
+                  displayed if you turn off certain permissions. You&apos;ll be
+                  prompted to update your permissions if needed.
+                </p>
+                <p>
+                  You can add or remove permissions later in the{" "}
+                  <a href="/settings" className="underline">
+                    settings
+                  </a>{" "}
+                  if you change your mind, or completely remove access using
+                  Fitbit&apos;s{" "}
+                  <a
+                    href="https://www.fitbit.com/settings/applications"
+                    target="_blank"
+                    className="underline"
+                  >
+                    linked apps
+                  </a>{" "}
+                  page.
+                </p>
+                <PermissionsTable />
+              </div>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+        <div className="mt-8 flex flex-col items-center">
+          <Button variant="contained" onClick={login}>
+            Connect with Fitbit
+          </Button>
+        </div>
+      </section>
     </Container>
   );
 }
