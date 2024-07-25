07282c1aa6469945a7190b6e1dda5f7e52d4b4f0
Jason Lai <jlai@users.noreply.github.com>
2024-07-24 18:25:23 -0700

    Update default dashboard layout

--- src/storage/tiles.ts
+++ src/storage/tiles.ts
@@ -11,7 +11,6 @@ export interface UserTile {
 
 const defaultTiles: Array<UserTile> = [
   { id: "default-gaugeSteps", type: "gaugeSteps", w: 2, h: 2, x: 0, y: 0 },
-
   {
     id: "default-gaugeCalories",
     type: "gaugeCaloriesBurned",
@@ -37,13 +36,35 @@ const defaultTiles: Array<UserTile> = [
     y: 3,
   },
   { id: "default-gaugeFloors", type: "gaugeFloors", w: 1, h: 1, x: 1, y: 3 },
-
   { id: "default-graph", type: "graph", w: 4, h: 2, x: 2, y: 0 },
-
-  { id: "default-water", type: "water", w: 2, h: 2, x: 2, y: 2 },
-  { id: "default-calorieGoal", type: "calorieGoal", w: 2, h: 2, x: 4, y: 2 },
+  { id: "default-water", type: "water", w: 1, h: 1, x: 2, y: 2 },
+  { id: "default-calorieGoal", type: "calorieGoal", w: 1, h: 1, x: 2, y: 3 },
   { id: "default-plant", type: "plant", w: 2, h: 2, x: 6, y: 0 },
   { id: "default-sleep", type: "sleep", w: 2, h: 2, x: 6, y: 2 },
+  {
+    id: "default-lifetimeSteps",
+    type: "lifetimeSteps",
+    w: 2,
+    h: 1,
+    x: 3,
+    y: 2,
+  },
+  {
+    id: "default-lifetimeDistance",
+    type: "lifetimeDistance",
+    w: 2,
+    h: 1,
+    x: 3,
+    y: 3,
+  },
+  {
+    id: "default-lifetimeFloors",
+    type: "lifetimeFloors",
+    w: 1,
+    h: 2,
+    x: 5,
+    y: 2,
+  },
 ];
 
 export const userTilesAtom = atomWithStorage(
