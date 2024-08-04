b1d2f26e04be81a478f6a3a294fb4d81a6f0dda2
Jason Lai <jlai@users.noreply.github.com>
2024-08-04 13:12:08 -0700

    Make default tile layout 1080p-friendly - Add default mobile layout

--- src/storage/tiles.ts
+++ src/storage/tiles.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 
@@ -38,40 +40,86 @@ const defaultTiles: Array<UserTile> = [
     y: 3,
   },
   { id: "default-gaugeFloors", type: "gaugeFloors", w: 1, h: 1, x: 1, y: 3 },
-  { id: "default-graph", type: "graph", w: 4, h: 2, x: 2, y: 0 },
+  { id: "default-graph", type: "graph", w: 3, h: 2, x: 2, y: 0 },
   { id: "default-water", type: "water", w: 1, h: 1, x: 2, y: 2 },
   { id: "default-calorieGoal", type: "calorieGoal", w: 1, h: 1, x: 2, y: 3 },
-  { id: "default-plant", type: "plant", w: 2, h: 2, x: 6, y: 0 },
-  { id: "default-sleep", type: "sleep", w: 2, h: 2, x: 6, y: 2 },
+  { id: "default-plant", type: "plant", w: 2, h: 2, x: 4, y: 2 },
+  { id: "default-sleep", type: "sleep", w: 1, h: 1, x: 3, y: 2 },
   {
     id: "default-lifetimeSteps",
     type: "lifetimeSteps",
-    w: 2,
+    w: 1,
     h: 1,
-    x: 3,
-    y: 2,
+    x: 5,
+    y: 0,
   },
   {
     id: "default-lifetimeDistance",
     type: "lifetimeDistance",
-    w: 2,
+    w: 1,
     h: 1,
-    x: 3,
-    y: 3,
+    x: 5,
+    y: 1,
+  },
+  { id: "default-heartRate", type: "heartRate", w: 1, h: 1, x: 3, y: 3 },
+];
+
+const defaultMobileTiles: Array<UserTile> = [
+  {
+    id: "default-gaugeSteps",
+    type: "gaugeSteps",
+    w: 1,
+    h: 1,
+    x: 0,
+    y: 0,
   },
   {
-    id: "default-lifetimeFloors",
-    type: "lifetimeFloors",
+    id: "default-gaugeCalories",
+    type: "gaugeCaloriesBurned",
     w: 1,
-    h: 2,
-    x: 5,
-    y: 2,
+    h: 1,
+    x: 0,
+    y: 1,
+    settings: { defaultTab: "pie" },
   },
+  {
+    id: "default-gaugeDistance",
+    type: "gaugeDistance",
+    w: 1,
+    h: 1,
+    x: 1,
+    y: 0,
+    settings: { defaultTab: "overview" },
+  },
+  {
+    id: "default-gaugeActiveMinutes",
+    type: "gaugeActiveMinutes",
+    w: 1,
+    h: 1,
+    x: 2,
+    y: 0,
+  },
+  {
+    id: "default-gaugeFloors",
+    type: "gaugeFloors",
+    w: 1,
+    h: 1,
+    x: 1,
+    y: 1,
+  },
+  { id: "default-graph", type: "graph", w: 2, h: 1, x: 0, y: 2 },
+  { id: "default-water", type: "water", w: 1, h: 1, x: 2, y: 1 },
+  { id: "default-heartRate", type: "heartRate", w: 1, h: 1, x: 0, y: 3 },
+  { id: "default-sleep", type: "sleep", w: 1, h: 1, x: 2, y: 2 },
+  { id: "default-plant", type: "plant", w: 2, h: 2, x: 1, y: 3 },
+  { id: "default-weight", type: "weight", w: 1, h: 1, x: 0, y: 4 },
 ];
+
+const isMobile = window.matchMedia("(max-width: 600px)").matches;
 
 export const userTilesAtom = atomWithStorage(
   "dashboard-tiles",
-  defaultTiles,
+  isMobile ? defaultMobileTiles : defaultTiles,
   undefined,
   {
     getOnInit: true,
