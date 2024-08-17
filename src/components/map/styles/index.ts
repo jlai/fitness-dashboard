cc00b2681a20907ce96ea065d0daf13c3b84e7e9
Jason Lai <jlai@users.noreply.github.com>
2024-08-16 17:15:59 -0700

    Add OpenStreetMap tile source

--- src/components/map/styles/index.ts
+++ src/components/map/styles/index.ts
@@ -3,6 +3,7 @@ import { MapStyle } from "react-map-gl/maplibre";
 import { MAPLIBRE_STYLE_URL } from "@/config";
 
 import opentopomapStyle from "./opentopomap.json";
+import openstreetmapStyle from "./openstreetmap.json";
 
 export const MAP_STYLE_OPTIONS: Array<{
   id: string;
@@ -21,8 +22,12 @@ export const MAP_STYLE_OPTIONS: Array<{
     label: "Dark",
   },
   {
+    id: "openstreetmap",
+    label: "OpenStreetMap",
+  },
+  {
     id: "opentopomap",
-    label: "Topography",
+    label: "OpenTopoMap",
   },
 ];
 
@@ -31,6 +36,8 @@ export function getMapStyle(name: string): string | MapStyle {
     case "light":
     case "white":
       return MAPLIBRE_STYLE_URL?.replace("{STYLE}", name) ?? "";
+    case "openstreetmap":
+      return openstreetmapStyle as MapStyle;
     case "opentopomap":
       return opentopomapStyle as MapStyle;
     default:
--- src/config/content-security-policy.ts
+++ src/config/content-security-policy.ts
@@ -4,6 +4,6 @@ export const CONTENT_SECURITY_POLICY = `
     default-src 'self';
     script-src 'self' 'unsafe-inline' blob: ${EXTRA_CSP_SCRIPT_SRC ?? ""};
     style-src 'self' 'unsafe-inline';
-    img-src 'self' data: https://asset-service.fitbit.com https://*.tile.opentopomap.org;
+    img-src 'self' data: https://asset-service.fitbit.com https://tile.openstreetmap.org https://*.tile.opentopomap.org;
     connect-src 'self' ${FITBIT_API_URL} https://api.protomaps.com https://protomaps.github.io
 `.replace(/\s+/, " ");
