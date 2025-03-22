187c243a21b3596117f13c6d8d43787cbccae9b8
Jason Lai <jlai@users.noreply.github.com>
2025-03-21 22:33:40 -0700

    Refactor hypnogram into smaller components for readability

--- src/components/sleep/hypnogram.tsx
+++ src/components/sleep/hypnogram.tsx
@@ -26,13 +26,205 @@ const levelIndex: Record<string, number> = {
   asleep: 3,
 };
 
+interface HypnogramProps {
+  sleepLog: SleepLog;
+  height: number | string;
+}
+
+interface SleepLevelPlotProps {
+  data: SleepLogLevelData[];
+  shortData: SleepLogLevelData[];
+  xScale: ScaleTime<number, number>;
+  yScale: ReturnType<typeof scaleBand<string>>;
+  height: number;
+  onMouseOver: (
+    event: React.MouseEvent<SVGElement>,
+    datum: SleepLogLevelData
+  ) => void;
+  onMouseOut: () => void;
+}
+
+interface HypnogramAxesProps {
+  xScale: ScaleTime<number, number>;
+  yScale: ReturnType<typeof scaleBand<string>>;
+  width: number;
+  height: number;
+  yAxisWidth: number;
+  xAxisHeight: number;
+}
+
+interface SleepSegmentTooltipContentProps {
+  tooltipData: SleepLogLevelData;
+}
+
+function SleepLevelPlot({
+  data,
+  shortData,
+  xScale,
+  yScale,
+  height,
+  onMouseOver,
+  onMouseOut,
+}: SleepLevelPlotProps) {
+  const lines: Array<React.ReactNode> = [];
+  const rects: Array<React.ReactNode> = [];
+
+  let lastEndX: number | undefined;
+  let lastY: number | undefined;
+
+  for (let i = 0; i < data.length; i++) {
+    const datum = data[i];
+    const startTime = dayjs(datum.dateTime);
+    const endTime = startTime.add(datum.seconds, "seconds");
+    const startX = xScale(startTime.toDate().getTime());
+    const endX = xScale(endTime.toDate().getTime());
+    const y = yScale(datum.level)!;
+
+    const currentLevelIndex = levelIndex[datum.level];
+    const prevLevelIndex = levelIndex[data[i - 1]?.level] ?? 0;
+    const nextLevelIndex = levelIndex[data[i + 1]?.level] ?? 0;
+
+    const startIsDeeper = currentLevelIndex > prevLevelIndex;
+    const endIsDeeper = nextLevelIndex > currentLevelIndex;
+
+    lines.push(
+      <BarRounded
+        key={datum.dateTime}
+        fill={LEVEL_COLORS[datum.level]}
+        radius={6}
+        x={startX}
+        y={y - 4}
+        width={endX - startX}
+        height={8}
+        topLeft={!startIsDeeper}
+        topRight={endIsDeeper}
+        bottomLeft={startIsDeeper}
+        bottomRight={!endIsDeeper}
+        onMouseMove={(event) => onMouseOver(event, datum)}
+        onMouseOut={onMouseOut}
+      />
+    );
+
+    // Add a transparent rect to catch mouse events
+    rects.push(
+      <rect
+        key={`rect-${datum.dateTime}`}
+        x={startX}
+        width={endX - startX}
+        y={0}
+        height={height}
+        fill="transparent"
+        onMouseMove={(event) => onMouseOver(event, datum)}
+        onMouseOut={onMouseOut}
+      />
+    );
+
+    // Add a line between the last end and the start of the current segment
+    if (lastEndX !== undefined && lastY !== undefined) {
+      lines.push(
+        <Line
+          key={`${datum.dateTime}-join`}
+          from={{ x: lastEndX, y: lastY }}
+          to={{ x: startX, y }}
+          stroke="rgb(195, 206, 224, 0.3)"
+          strokeWidth={1}
+        />
+      );
+    }
+
+    lastEndX = endX;
+    lastY = y;
+  }
+
+  // Add lines for short data
+  for (let i = 0; i < shortData.length; i++) {
+    const datum = shortData[i];
+    const startTime = dayjs(datum.dateTime);
+    const endTime = startTime.add(datum.seconds, "seconds");
+    const startX = xScale(startTime.toDate());
+    const endX = xScale(endTime.toDate());
+    const y = yScale(datum.level);
+
+    lines.push(
+      <Line
+        key={`${datum.dateTime}-short`}
+        stroke={LEVEL_COLORS[datum.level]}
+        strokeWidth={8}
+        from={{ x: startX, y }}
+        to={{ x: endX, y }}
+        onMouseMove={(event) => onMouseOver(event, datum)}
+        onMouseOut={onMouseOut}
+      />
+    );
+  }
+
+  return (
+    <>
+      <Group>{rects}</Group>
+      <Group>{lines}</Group>
+    </>
+  );
+}
+
+function HypnogramAxes({
+  xScale,
+  yScale,
+  width,
+  height,
+  yAxisWidth,
+  xAxisHeight,
+}: HypnogramAxesProps) {
+  return (
+    <>
+      <AxisLeft
+        left={yAxisWidth}
+        scale={yScale}
+        stroke="#cbd5e1"
+        strokeWidth={0}
+        hideTicks
+        tickFormat={(value: string) => LEVEL_NAMES[value]}
+        tickLabelProps={{ fill: "#cbd5e1", fontSize: 14 }}
+      />
+      <AxisBottom<ScaleTime<number, number>>
+        left={0}
+        top={height - xAxisHeight}
+        scale={xScale}
+        stroke="#cbd5e1"
+        strokeWidth={0}
+        numTicks={width > 500 ? undefined : 4}
+        tickStroke="#cbd5e1"
+        tickLineProps={{ strokeWidth: 1 }}
+        tickLabelProps={{ fill: "#cbd5e1", fontSize: 14 }}
+      />
+    </>
+  );
+}
+
+function SleepSegmentTooltipContent({
+  tooltipData: { level, seconds, dateTime },
+}: SleepSegmentTooltipContentProps) {
+  const startTime = new Date(dateTime);
+  const endTime = new Date(startTime.getTime() + seconds * 1000);
+
+  return (
+    <div className="space-y-1">
+      <Stack direction="row" alignItems="center" columnGap={1}>
+        <Box width="1em" height="1em" bgcolor={LEVEL_COLORS[level]}></Box>
+        <Typography>
+          {LEVEL_NAMES[level]}: <b>{formatSeconds(seconds)}</b>
+        </Typography>
+      </Stack>
+      <Typography>
+        {TIME.format(startTime)} &ndash; {TIME.format(endTime)}
+      </Typography>
+    </div>
+  );
+}
+
 export function Hypnogram({
   sleepLog,
   height: containerHeight,
-}: {
-  sleepLog: SleepLog;
-  height: number | string;
-}) {
+}: HypnogramProps) {
   const { parentRef, width, height } = useParentSize();
   const {
     showTooltip,
@@ -90,95 +282,6 @@ export function Hypnogram({
   const data = sleepLog.levels?.data ?? [];
   const shortData = sleepLog.levels?.shortData ?? [];
 
-  const lines: Array<React.ReactNode> = [];
-  const rects: Array<React.ReactNode> = [];
-
-  let lastEndX: number | undefined;
-  let lastY: number | undefined;
-
-  for (let i = 0; i < data.length; i++) {
-    const datum = data[i];
-    const startTime = dayjs(datum.dateTime);
-    const endTime = startTime.add(datum.seconds, "seconds");
-    const startX = xScale(startTime.toDate().getTime());
-    const endX = xScale(endTime.toDate().getTime());
-    const y = yScale(datum.level)!;
-
-    const currentLevelIndex = levelIndex[datum.level];
-    const prevLevelIndex = levelIndex[data[i - 1]?.level] ?? 0;
-    const nextLevelIndex = levelIndex[data[i + 1]?.level] ?? 0;
-
-    const startIsDeeper = currentLevelIndex > prevLevelIndex;
-    const endIsDeeper = nextLevelIndex > currentLevelIndex;
-
-    lines.push(
-      <BarRounded
-        key={datum.dateTime}
-        fill={LEVEL_COLORS[datum.level]}
-        radius={6}
-        x={startX}
-        y={y - 4}
-        width={endX - startX}
-        height={8}
-        topLeft={!startIsDeeper}
-        topRight={endIsDeeper}
-        bottomLeft={startIsDeeper}
-        bottomRight={!endIsDeeper}
-        onMouseMove={(event) => handleMouseOver(event, datum)}
-        onMouseOut={hideTooltip}
-      />
-    );
-
-    rects.push(
-      <rect
-        key={`rect-${datum.dateTime}`}
-        x={startX}
-        width={endX - startX}
-        y={0}
-        height={height}
-        fill="transparent"
-        onMouseMove={(event) => handleMouseOver(event, datum)}
-        onMouseOut={hideTooltip}
-      />
-    );
-
-    if (lastEndX !== undefined && lastY !== undefined) {
-      lines.push(
-        <Line
-          key={`${datum.dateTime}-join`}
-          from={{ x: lastEndX, y: lastY }}
-          to={{ x: startX, y }}
-          stroke="rgb(195, 206, 224, 0.3)"
-          strokeWidth={1}
-        />
-      );
-    }
-
-    lastEndX = endX;
-    lastY = y;
-  }
-
-  for (let i = 0; i < shortData.length; i++) {
-    const datum = shortData[i];
-    const startTime = dayjs(datum.dateTime);
-    const endTime = startTime.add(datum.seconds, "seconds");
-    const startX = xScale(startTime.toDate());
-    const endX = xScale(endTime.toDate());
-    const y = yScale(datum.level);
-
-    lines.push(
-      <Line
-        key={`${datum.dateTime}-short`}
-        stroke={LEVEL_COLORS[datum.level]}
-        strokeWidth={8}
-        from={{ x: startX, y }}
-        to={{ x: endX, y }}
-        onMouseMove={(event) => handleMouseOver(event, datum)}
-        onMouseOut={hideTooltip}
-      />
-    );
-  }
-
   return (
     <div
       ref={parentRef}
@@ -199,27 +302,22 @@ export function Hypnogram({
           from="#3b6978"
           to="#204051"
         />
-        <Group>{rects}</Group>
-        <Group>{lines}</Group>
-        <AxisLeft
-          left={yAxisWidth}
-          scale={yScale}
-          stroke="#cbd5e1"
-          strokeWidth={0}
-          hideTicks
-          tickFormat={(value) => LEVEL_NAMES[value]}
-          tickLabelProps={{ fill: "#cbd5e1", fontSize: 14 }}
+        <SleepLevelPlot
+          data={data}
+          shortData={shortData}
+          xScale={xScale}
+          yScale={yScale}
+          height={height}
+          onMouseOver={handleMouseOver}
+          onMouseOut={hideTooltip}
         />
-        <AxisBottom<ScaleTime<number, number>>
-          left={0}
-          top={height - xAxisHeight}
-          scale={xScale}
-          stroke="#cbd5e1"
-          strokeWidth={0}
-          numTicks={width > 500 ? undefined : 4}
-          tickStroke="#cbd5e1"
-          tickLineProps={{ strokeWidth: 1 }}
-          tickLabelProps={{ fill: "#cbd5e1", fontSize: 14 }}
+        <HypnogramAxes
+          xScale={xScale}
+          yScale={yScale}
+          width={width}
+          height={height}
+          yAxisWidth={yAxisWidth}
+          xAxisHeight={xAxisHeight}
         />
       </svg>
       {tooltipOpen && tooltipData && (
@@ -231,29 +329,6 @@ export function Hypnogram({
           <SleepSegmentTooltipContent tooltipData={tooltipData} />
         </TooltipInPortal>
       )}
-    </div>
-  );
-}
-
-function SleepSegmentTooltipContent({
-  tooltipData: { level, seconds, dateTime },
-}: {
-  tooltipData: SleepLogLevelData;
-}) {
-  const startTime = new Date(dateTime);
-  const endTime = new Date(startTime.getTime() + seconds * 1000);
-
-  return (
-    <div className="space-y-1">
-      <Stack direction="row" alignItems="center" columnGap={1}>
-        <Box width="1em" height="1em" bgcolor={LEVEL_COLORS[level]}></Box>
-        <Typography>
-          {LEVEL_NAMES[level]}: <b>{formatSeconds(seconds)}</b>
-        </Typography>
-      </Stack>
-      <Typography>
-        {TIME.format(startTime)} &ndash; {TIME.format(endTime)}
-      </Typography>
     </div>
   );
 }
